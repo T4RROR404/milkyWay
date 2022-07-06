ea08c7107b178db8dff0447ff75f1719ef54c23d
insomnia <talkers-maximsOf@icloud.com>
2022-07-06 22:58:54 +0300

    added physics, logic to the shot, made same edits to code

--- milkyWay/GameScene.swift
+++ milkyWay/GameScene.swift
@@ -8,10 +8,18 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let player = SKSpriteNode(imageNamed: "playerShip")
     let shotSound = SKAction.playSoundFileNamed("shot", waitForCompletion: false)
+    let explosionSound = SKAction.playSoundFileNamed("explosion", waitForCompletion: false)
+    
+    struct PhysicsCategories {
+        static let None: UInt32 = 0
+        static let Player: UInt32 = 0b1
+        static let Shot: UInt32 = 0b10
+        static let Enemy: UInt32 = 0b100
+    }
     
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / Float(0xFFFFFFFF))
@@ -20,6 +28,8 @@ class GameScene: SKScene {
     func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max - min) + min
     }
+    
+    
     
     var gameArea: CGRect
     
@@ -40,6 +50,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        self.physicsWorld.contactDelegate = self
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
         background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
@@ -50,9 +61,68 @@ class GameScene: SKScene {
         player.setScale(1)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.1)
         player.zPosition = 2
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody!.affectedByGravity = false
+        player.physicsBody!.categoryBitMask = PhysicsCategories.Player
+        player.physicsBody!.collisionBitMask = PhysicsCategories.None
+        player.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(player)
         
         startNewLevel()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var body1 = SKPhysicsBody()
+        var body2 = SKPhysicsBody()
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            body1 = contact.bodyA
+            body2 = contact.bodyB
+        } else {
+            body1 = contact.bodyB
+            body2 = contact.bodyA
+        }
+        
+        if body1.categoryBitMask == PhysicsCategories.Player && body2.categoryBitMask == PhysicsCategories.Enemy {
+            
+            if body1.node != nil {
+                spawnExplosion(spawnPosition: body1.node!.position)
+            }
+            if body2.node != nil {
+                spawnExplosion(spawnPosition: body2.node!.position)
+            }
+            
+            
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+        }
+        
+        if body1.categoryBitMask == PhysicsCategories.Shot && body2.categoryBitMask == PhysicsCategories.Enemy && (body2.node?.position.y)! < self.size.height {
+            
+            if body2.node != nil {
+                spawnExplosion(spawnPosition: body2.node!.position)
+            }
+            
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+            
+        }
+        
+    }
+    
+    func spawnExplosion(spawnPosition: CGPoint) {
+        let explosion = SKSpriteNode(imageNamed: "explosion")
+        explosion.position = spawnPosition
+        explosion.zPosition = 3
+        explosion.setScale(0)
+        self.addChild(explosion)
+        
+        let scaleIn = SKAction.scale(to: 1, duration: 0.1)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.1)
+        let delete = SKAction.removeFromParent()
+        
+        let explosionSequence = SKAction.sequence([explosionSound, scaleIn, fadeOut, delete])
+        explosion.run(explosionSequence)
     }
     
     func startNewLevel() {
@@ -70,6 +140,11 @@ class GameScene: SKScene {
         shot.setScale(0.5)
         shot.position = player.position
         shot.zPosition = 1
+        shot.physicsBody = SKPhysicsBody(rectangleOf: shot.size)
+        shot.physicsBody!.affectedByGravity = false
+        shot.physicsBody!.categoryBitMask = PhysicsCategories.Shot
+        shot.physicsBody!.collisionBitMask = PhysicsCategories.None
+        shot.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(shot)
         
         let moveBullet = SKAction.moveTo(y: self.size.height + shot.size.height, duration: 1)
@@ -89,6 +164,11 @@ class GameScene: SKScene {
         enemy.setScale(1)
         enemy.position = statrPoint
         enemy.zPosition = 2
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody!.affectedByGravity = false
+        enemy.physicsBody!.categoryBitMask = PhysicsCategories.Enemy
+        enemy.physicsBody!.collisionBitMask = PhysicsCategories.None
+        enemy.physicsBody!.contactTestBitMask = PhysicsCategories.Player | PhysicsCategories.Shot
         self.addChild(enemy)
         
         let moveEnemy = SKAction.move(to: endPoint, duration: 1.5)
